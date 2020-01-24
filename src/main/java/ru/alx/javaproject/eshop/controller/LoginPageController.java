0dab5b2b0c55b7d209507ec757543a59b504350a
Vasilenko, Oleksandr <oleksandr.vasilenko@accenture.com>
2020-01-24 18:32:16 +0100

    Login page updated | User entity | DB update

--- src/main/java/ru/alx/javaproject/eshop/controller/LoginPageController.java
+++ src/main/java/ru/alx/javaproject/eshop/controller/LoginPageController.java
@@ -1,29 +1,56 @@
 package ru.alx.javaproject.eshop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import ru.alx.javaproject.eshop.entity.User;
+import ru.alx.javaproject.eshop.service.UserValidator;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.core.Context;
 import java.time.LocalDate;
 
 @Controller
 public class LoginPageController {
+
+    @Autowired
+    UserValidator userValidator;
+
+    @PersistenceContext
+    EntityManager em;
 
     @RequestMapping("/")
     public ModelAndView loginPageLoader (){
         ModelAndView modelAndView = new ModelAndView("LoginPage");
 
         modelAndView.addObject("isAdmin", "true");
-
         return modelAndView;
 
     }
 
-    @RequestMapping("/login")
-    public ModelAndView loginRedirectionPageLoader (){
-        ModelAndView modelAndView = new ModelAndView ("redirect:/Main");
-        return
+    @RequestMapping("/Login")
+    public ModelAndView RedirectionPageLoader (@ModelAttribute ("user") User user){
+        ModelAndView modelAndView = new ModelAndView ();
+
+        if(userValidator.checkUserAuthorization(user)){
+            modelAndView.setViewName("redirect:/MainPage");
+        } else {
+            modelAndView.setViewName("redirect:/Login");
+            modelAndView.addObject("error", true);
+        }
+        return modelAndView;
     }
+
+
+    @RequestMapping("/Registration")
+    public ModelAndView RegistrationPageLoader (){
+        ModelAndView modelAndView = new ModelAndView ("redirect:/RegistrationPage");
+        return modelAndView;
+    }
+
 
 
 
